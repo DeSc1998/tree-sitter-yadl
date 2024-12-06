5cad8a07ec307fffb42fa7f6deb9cad505c4db7a
Dennis Schulze <dennis.benedikt.schulze@proton.me>
2024-12-06 19:30:08 +0100

    added query

---
+++ queries/highlights.scm
@@ -0,0 +1,12 @@
+; highlights.scm
+
+"if" @keyword
+"elif" @keyword
+"else" @keyword
+"return" @keyword
+"while" @keyword
+(number) @number
+(string) @string
+(assignment
+  (identifier) @function
+  (function_expression))
